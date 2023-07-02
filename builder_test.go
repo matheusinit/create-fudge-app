9c8edc3857639afa052253dfe5c63a8b3a942f03
matheusinit <matheus.oliveira.s@protonmail.com>
2023-07-02 13:00:04 -0300

    test: ensure function getAppName returns proper value when only relative path is passed

--- builder_test.go
+++ builder_test.go
@@ -41,3 +41,13 @@ func TestIfGetAppNameReturnsProperValueAtDefaultValue(t *testing.T) {
 		t.Errorf("getAppName(installation_path) = %q, want %q", got, expected)
 	}
 }
+
+func TestIfGetAppNameReturnsProperValueAtOnlyRelativePath(t *testing.T) {
+	installation_path := "../.."
+
+	expected := "app"
+
+	if got := getAppName(installation_path); got != expected {
+		t.Errorf("getAppName(installation_path) = %q, want %q", got, expected)
+	}
+}
